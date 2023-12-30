63d3936e7a3beb6b86229cd56953749aed0fdefb
Jack Spagnoli <JackSpagnoli@gmail.com>
2023-12-30 19:11:00 +0000

    Update header styling and text alignment

--- src/header.rs
+++ src/header.rs
@@ -3,21 +3,21 @@ use leptos::{IntoView, component, view};
 #[component]
 pub fn Header() -> impl IntoView {
     view! {
-        <div id="Header" class="transition-h duration-300 items-center justify-center h-24 hover:h-32 w-full flex flex-row">
-            <div class="w-10"></div>
-            <div class="flex flex-row w-1/3">
+        <div id="Header" class="transition-h duration-300 items-center justify-center h-32 hover:h-36 w-full flex flex-row">
+            <div class="w-10"/>
+            <div class="flex flex-row w-1/3 items-center">
                 <img
                     class="w-16 h-16 rounded-full"
                     src="https://avatars.githubusercontent.com/u/114365987?v=4"
                 />
-                <div class="w-4"></div>
-                <h1 class="text-5xl">"Jack Spagnoli"</h1>
+                <div class="w-4"/>
+                <h1 class="text-4xl align-middle">"Jack Spagnoli"</h1>
             </div>
-            <div class="w-1/6"></div>
-            <div class="align-middle w-1/2 justify-center flex items-center">
+            <div class="w-1/6"/>
+            <div class="align-middle w-1/6 text-center">
                 <h2>"Impressive Personal Website"</h2>
             </div>
-            <div class="w-1/6"></div>
+            <div class="w-1/6"/>
             <div class="flex flex-row w-1/3 justify-end">
                 <a href="https://gitea.spagnoli.co.uk/Jack/PersonalWebsite">
                     <div
@@ -26,7 +26,7 @@ pub fn Header() -> impl IntoView {
                     />
                 </a>
             </div>
-            <div class="w-10"></div>
+            <div class="w-10"/>
         </div>
     }
 }
